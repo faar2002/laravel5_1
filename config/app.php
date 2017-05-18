f7cd4e6c205f9c77074697287cecfa6737b4017d
faar2002 <faar2002@gmail.com>
2017-05-18 19:18:13 -0300

    Cambio de lenguaje de la aplicacion

--- config/app.php
+++ config/app.php
@@ -107,6 +107,8 @@ return [
     | this array to grant expanded functionality to your applications.
     |
     */
+    
+    Collective\Html\HtmlServiceProvider::class,
 
     'providers' => [
 
@@ -192,7 +194,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+        'Form' => 'Collective\Html\FormFacade',
+        'Html' => 'Collective\Html\HtmlFacade',
     ],
 
 ];
